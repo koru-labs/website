17bef60da0e674564e49c97135d8b6557a19c640
wenhao <wenhao@hamsa.com>
2025-05-12 15:06:06 -0700

    save before commit

--- contracts/ucl/util/HamsaInternalEvents.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract HamsaInternalEvents {
-    event BundleExpire(uint256, uint256);
-    event BundleFill(uint256, uint256);
-    event BundleCancel(uint256, uint256);
-}
