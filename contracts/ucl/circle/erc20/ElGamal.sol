f3ce28c68c869605b59312770d15704fdc600f1f
Aldenio Burgos <aldenio.burgos@hamsa.com>
2025-05-13 10:17:14 -0400

    Add ElGamal struct to ElGamal.sol.

--- contracts/ucl/circle/erc20/ElGamal.sol
+++ contracts/ucl/circle/erc20/ElGamal.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.8.0;
 
-
-
-
+    struct ElGamal {
+        uint256 cl_x;
+        uint256 cl_y;
+        uint256 cr_x;
+        uint256 cr_y;
+    }
 
     struct Allowance {
         ElGamal amount;
