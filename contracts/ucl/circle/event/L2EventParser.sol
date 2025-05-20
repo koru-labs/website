9d59c13afc57a3fc9bf71f134499d2780ea0bb7b
chenkun <kun.chen@hamsapay.com>
2025-05-20 10:27:05 +0800

    fix bug

--- contracts/ucl/circle/event/L2EventParser.sol
+++ contracts/ucl/circle/event/L2EventParser.sol
@@ -6,6 +6,9 @@ import "./L2EventDefinitions.sol";
 contract L2EventParser {
     function parseTokenSCCreated(TokenSCCreatedEvent memory e) public view {}
     function parseTokenDetail(TokenDetailBody memory e) public view {}
+    function parseInstitutionRegistered(InstitutionRegisteredEvent memory e) public view {}
+    function parseUserRegistered(UserRegisteredEvent memory e) public view {}
+    function parseTokenMinted(TokenMintedEvent memory e) public view {}
     function parseTokenMeta(TokenMetaBody memory e) public view { }
     function parseTokenRemovedEvent(TokenRemovedEvent memory e) public view { }
 }
