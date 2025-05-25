c03569dd8c00ce5827f950901e531d2199668711
wenhao <wenhao@hamsa.com>
2025-05-24 21:54:26 -0700

    [ucl-902-3] delete not needed test cases

--- script/image9/config.js
+++
@@ -1,6 +0,0 @@
-// Contract address configuration - modify according to actual deployment
-const CONTRACT_CONFIG = {
-  tokenScBase: "0x63E4c1C77Cd96D62f2819a2C794a3944b3F3622d",
-};
-
-module.exports = { CONTRACT_CONFIG };
