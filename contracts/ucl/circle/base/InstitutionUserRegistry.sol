c27afe9fdaf68418151ce060a9befcf02158590f
coastsee <mengcheng.xu@hamsapy.com>
2025-06-04 17:31:59 +0800

    remove institutionPublicKey map

--- contracts/ucl/circle/base/InstitutionUserRegistry.sol
+++ contracts/ucl/circle/base/InstitutionUserRegistry.sol
@@ -7,26 +7,18 @@ import "../event/IL2Event.sol";
 contract InstitutionUserRegistry {
     address public owner;
     IL2Event public l2Event;
-    
-    // institution:
-    // name
-    // managerAddress
-    // publicKey
+
     struct Institution {
         string name;
         address managerAddress;
         TokenModel.GrumpkinPublicKey publicKey;
     }
-    
-    // user:
-    // address
-    // managerAddress
+
     struct User {
         address userAddress;
         address managerAddress;
     }
-    
-    mapping(address => TokenModel.GrumpkinPublicKey) public institutionPublicKey;
+
     mapping(address => Institution) public institutions;
     mapping(address => User) public users;
     mapping(address => address) public userToManager;
@@ -52,7 +44,6 @@ contract InstitutionUserRegistry {
         });
         
         institutions[institutionAddress] = institution;
-        institutionPublicKey[institutionAddress] = publicKey;
         
         TokenEventLib.triggerInstitutionRegisteredEvent(
             l2Event,
@@ -77,7 +68,6 @@ contract InstitutionUserRegistry {
         
         users[userAddress] = user;
         userToManager[userAddress] = managerAddress;
-        managerToUsers[managerAddress].push(userAddress);
         
         TokenEventLib.triggerUserRegisteredEvent(
             l2Event,
@@ -100,7 +90,6 @@ contract InstitutionUserRegistry {
         
         users[msg.sender] = user;
         userToManager[msg.sender] = managerAddress;
-        managerToUsers[managerAddress].push(msg.sender);
         
         TokenEventLib.triggerUserRegisteredEvent(
             l2Event,
@@ -119,22 +108,14 @@ contract InstitutionUserRegistry {
         return institutions[managerAddress];
     }
     
-    function getManagerUsers(address managerAddress) public view returns (address[] memory) {
-        return managerToUsers[managerAddress];
-    }
-    
     function transferOwnership(address newOwner) external onlyOwner {
         require(newOwner != address(0), "Invalid new owner address");
         owner = newOwner;
     }
 
-    function getInstGrumpkinPubKey(address institutionAddress) public view returns (TokenModel.GrumpkinPublicKey memory) {
-        return institutionPublicKey[institutionAddress];
-    }
-
     function getUserInstGrumpkinPubKey(address userAddress) public view returns (TokenModel.GrumpkinPublicKey memory) {
         address institutionAddress = getUserManager(userAddress);
-        return institutionPublicKey[institutionAddress];
+        return institutions[institutionAddress].publicKey;
     }
 
 }
