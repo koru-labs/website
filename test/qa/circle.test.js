807c173fd4c56c10d02acc4444a2b8b9a089e5aa
jiaohui <jiaohui.shi@hamsapay.com>
2025-06-12 18:16:44 +0800

    test: 74 passed

--- test/qa/circle.test.js
+++ test/qa/circle.test.js
@@ -6,8 +6,8 @@ const accounts = require('./../../deployments/account.json');
 const {createClient} = require('../qa/token_grpc')
 
 
-const rpcUrl = "sb-node3-node.hamsa-ucl.com:50051"
-const rpcUrl_1 = "sb-node1-node.hamsa-ucl.com:50051"
+const rpcUrl = "sb-node3-rpc.hamsa-ucl.com:50051"
+const rpcUrl_1 = "sb-node1-rpc.hamsa-ucl.com:50051"
 // const rpcUrl_1 = "a06220caa131f4da982beeebdd84ffc5-576531359.us-west-1.elb.amazonaws.com:50051"
 // const rpcUrl = 'dev-node3-node.hamsa-ucl.com:50051'
 const client = createClient(rpcUrl)
@@ -75,6 +75,7 @@ async function mint(address,amount) {
         const response = await client.generateMintProof(generateRequest);
         const proofResult = await client.waitForProofCompletion(client.getMintProof, response.request_id)
         const receipt = await callPrivateMint(config.contracts.PrivateERCToken, proofResult, minterWallet)
+        await sleep(1000)
         return  receipt
     }catch (error){
         const wrappedError = new Error('Minting failed: ' + error.details);
@@ -513,9 +514,9 @@ describe("ReserveTokensAndTransfer",  function (){
     this.timeout(1200000);
     let preBalanceTo,postBalanceTo;
     before(async function () {
-        const mintAmount = amount * 13
-        await mint(accounts.Minter,mintAmount);
-        await mint(accounts.To1,100);
+        // const mintAmount = amount * 13
+        await DirectMint(accounts.Minter,1300);
+        await DirectMint(accounts.To1,100);
     });
 
     beforeEach(async function () {
@@ -625,7 +626,6 @@ describe("ReserveTokensAndTransfer",  function (){
         }
     });
     it('transfer_all_amount',async () => {
-        await cancelAllSplitTokens(minterWallet,config.contracts.PrivateERCToken);
         const amount = await getTokenBalance(accounts.Minter);
         console.log("minter amount:",amount)
         preBalanceTo = await getTokenBalance(toAddress1);
