9632278e0fa0c1840de62bfafae8138a371d5c15
yuanqianwei <qianwei.yuan@hamsapay.com>
2025-07-02 14:22:13 +0800

    ucl-691:test

--- test/qianwei/authorization_test.js
+++ test/qianwei/authorization_test.js
@@ -9,11 +9,11 @@ const client = createClient(rpcUrl);
 // admin
 const privateKey = "555332672ce947d150d23a36bf3847078291f89bda7073829bb718c77d626787";
 // normal
-// const privateKey = "2b42ed39b2d9c3d576320af626b90a62ce726ee0f25764061947891415dbe782";
+// const privateKey = "518eb784dd768d8c0cdf9218d44ae8f498d0cadf7ecf98f5ecf27c6b793986ca";//0x4568E35F2c4590Bde059be615015AaB6cc873004
 
 // const address = "0x8c8af239FfB9A6e93AC4b434C71a135572A1021C";
 // const address = "0x4312488937D47A007De24d48aB82940C809EEb2b";
-const address = "0x4312488937D47A007De24d48aB82940C809EEb2b";//test
+const address = "0x4568E35F2c4590Bde059be615015AaB6cc873004";//test
 
 async function createAuthMetadata(privateKey, messagePrefix = "login") {
     const wallet = new ethers.Wallet(privateKey);
@@ -29,6 +29,10 @@ async function createAuthMetadata(privateKey, messagePrefix = "login") {
     return metadata;
 }
 
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function testUpdateAccountStatus() {
     try {
         const metadata = await createAuthMetadata(privateKey);
@@ -40,10 +44,10 @@ async function testUpdateAccountStatus() {
         const response = await client.updateAccountStatus(request, metadata);
         console.log("Success:", response);
         if (response.status !== "ASYNC_ACTION_STATUS_FAIL") {
+            await delay(10000);
             const actionRequest = {
                 request_id: response.request_id,
             };
-
             const actionResponse = await client.getAsyncAction(actionRequest, metadata);
             console.log("action response:", actionResponse);
         }
@@ -66,7 +70,7 @@ async function testRegisterAccount() {
             const actionRequest = {
                 request_id: response.request_id,
             };
-
+            await delay(10000);
             const actionResponse = await client.getAsyncAction(actionRequest, metadata);
             console.log("action response:", actionResponse);
         }
