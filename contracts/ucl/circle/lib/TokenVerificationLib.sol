ec4ff1a4a5ad4605b9acf276460b390f06f94da0
coastsee <mengcheng.xu@hamsapy.com>
2025-07-08 16:32:11 +0800

    remove TokenVerificationLib.sol

--- contracts/ucl/circle/lib/TokenVerificationLib.sol
+++ contracts/ucl/circle/lib/TokenVerificationLib.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "../base/InstitutionUserRegistry.sol";
 import "../model/TokenModel.sol";
-import "./verify/BurnTokenVerifier.sol";
 import "./verify/MintAllowedTokenVerifier.sol";
 import "./verify/SplitTokenVerifier.sol";
 import "./verify/SplitAllowanceTokenVerifier.sol";
