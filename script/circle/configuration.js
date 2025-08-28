e8d386b11cac8bfc242dc005fd21184ec7abf224
yuanqianwei <qianwei.yuan@hamsapay.com>
2025-08-28 09:59:26 +0800

    update 0xf17f52151EbEF6C7334FAD080c5704D77216b732 only is admin

--- script/circle/configuration.js
+++ script/circle/configuration.js
@@ -55,7 +55,7 @@ module.exports = {
             privateKey: "2607683766450702001126943055270332377994929386369594371567962723856157825017",
             users: [
                 {address: "0xe46fe251dd1d9ffc247bc0ddb6d61e4ee4416ecb", role:"minter"},
-                {address: "0xf17f52151EbEF6C7334FAD080c5704D77216b732", role:"admin,minter"},
+                {address: "0xf17f52151EbEF6C7334FAD080c5704D77216b732", role:"admin"},
                 {address: "0xf0b6C36D47f82Fc13eFEE4CC8223Dc19E6c0D766", role:"normal"},
                 {address: "0x8c8af239FfB9A6e93AC4b434C71a135572A1021C", role:"normal"},
                 {address: "0x4312488937D47A007De24d48aB82940C809EEb2b", role:"normal"},
