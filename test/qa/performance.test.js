2286a3b39acf938c59d3f191282e47eb43449c37
jiaohui <jiaohui.shi@hamsapay.com>
2025-09-10 10:37:42 +0800

    test:privateTransfers tps

--- test/qa/performance.test.js
+++ test/qa/performance.test.js
@@ -206,7 +206,7 @@ async function processBatch(batchSignedTxs, batchMetadata, batchIndex, totalBatc
 describe.only("Performance Test with created 10 minters", function () {
     this.timeout(120000000);
 
-    const TOTAL_SIZE = 500;
+    const TOTAL_SIZE = 1000;
     const BATCH_SIZE = 1000;
 
     const minter1 = ethers.Wallet.createRandom();
