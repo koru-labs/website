ff308e491c01a26407c5a8d2782d1f9d39eb8f9d
yuanqianwei <qianwei.yuan@hamsapay.com>
2025-09-23 22:09:27 +0800

    dev:redeploy contract

--- script/circle/infra/deploy_L1_rollup.js
+++ script/circle/infra/deploy_L1_rollup.js
@@ -3,22 +3,17 @@ const {ethers} = require("hardhat");
 
 async function deployHandle() {
     const [ownerWallet] = await ethers.getSigners();
-
-    
     const Classification = await ethers.getContractFactory("contracts/ucl/l1verify/Classification.sol:Classification",ownerWallet);
     const classification = await Classification.deploy();
     await classification.waitForDeployment();
-    console.log("Classification is deploy at :", await classification.getAddress())
-    
+
     const TokenUtilsLib = await ethers.getContractFactory("contracts/ucl/l1verify/lib/TokenUtilsLib.sol:TokenUtilsLib",ownerWallet);
     const tokenUtilsLib = await TokenUtilsLib.deploy();
     await tokenUtilsLib.waitForDeployment();
-    console.log("TokenUtilsLib is deploy at :", await tokenUtilsLib.getAddress())
 
     const Verifier = await ethers.getContractFactory("contracts/ucl/l1verify/lib/verify/Verifier.sol:Verifier",ownerWallet);
     const verifier = await Verifier.deploy();
     await verifier.waitForDeployment();
-    console.log("Verifier is deploy at :", await verifier.getAddress())
 
     // deploy Handle
     const Handle = await ethers.getContractFactory("Handle", {
@@ -31,20 +26,9 @@ async function deployHandle() {
     
     const handle = await Handle.deploy();
     await handle.waitForDeployment();
-    
-    console.log("handle contract deploy done");
-    console.log("Handle is deploy at :", await handle.getAddress())
 
+    console.log("L1RollupVerifyscAddress is deploy at:", await handle.getAddress());
 
-    console.log("=====================================");
-    console.log("network:", network.name);
-    console.log("Deployer address:", ownerWallet.address);
-    console.log("Classification address:", await classification.getAddress());
-    console.log("TokenUtilsLib address:", await tokenUtilsLib.getAddress());
-    console.log("Verifier address:", await verifier.getAddress());
-    console.log("Handle address:", await handle.getAddress());
-    console.log("=====================================");
-    
     return handle;
 }
 
@@ -63,10 +47,5 @@ async function deployL1Contract() {
     // and change the contract BlobCommitmentVerify.sol.bak to BlobCommitmentVerify.sol
     await deployBlobCommitmentVerify();
 }
-
-
-
-// deployHandle().then();
-// deployBlobCommitmentVerify().then();
 
 deployL1Contract().then();
