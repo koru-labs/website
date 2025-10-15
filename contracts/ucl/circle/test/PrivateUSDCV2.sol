01b2895e966988b259561c548c0bf8fe868a84c8
coastsee <mengcheng.xu@hamsapy.com>
2025-10-15 16:13:25 +0800

    Fix the private USDC import path in the test contract

--- contracts/ucl/circle/test/PrivateUSDCV2.sol
+++ contracts/ucl/circle/test/PrivateUSDCV2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./PrivateUSDC.sol";
+import "../PrivateUSDC.sol";
 
 /**
  * @dev Extended implementation used for testing proxy routing.
