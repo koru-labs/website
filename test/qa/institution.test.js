8d5809ef89fe951e9a2e14a59d5b2e867e2630bf
jiaohui <jiaohui.shi@hamsapay.com>
2025-11-12 18:32:06 +0800

    test institution

---
+++ test/qa/institution.test.js
@@ -0,0 +1,140 @@
+// test/PrivateToken.test.js
+const { expect } = require('chai');
+const { ethers, network} = require('hardhat');
+const { TestConfig } = require('../config/TestConfig');
+const { TokenTestHelper,TokenType, CommentType } = require('../help/TokenTestHelper');
+const accounts = require('../../deployments/account.json');
+const hardhatConfig = require("../../hardhat.config");
+
+const l1CustomNetwork = {
+    name: "BESU",
+    chainId: 1337
+};
+
+const providerOptions = {
+    batchMaxCount: 1,
+    staticNetwork: true
+};
+async function getProvider() {
+    const networkName = network.name;
+    let l1Url;
+
+    if (hardhatConfig.networks[networkName] && hardhatConfig.networks[networkName].url) {
+        l1Url = hardhatConfig.networks[networkName].url;
+    }
+
+    if (!l1Url) {
+        throw new Error(`No RPC url configured for current network: ${networkName}`);
+    }
+
+    console.log("L1Url:", l1Url);
+    return new ethers.JsonRpcProvider(l1Url, l1CustomNetwork, providerOptions);
+}
+
+
+describe('Institution Cases', function () {
+    this.timeout(1200000);
+
+    let testConfig;
+    let helper;
+    let metadata;
+    let registryContract;
+    let l1Provider;
+    let proxyAddress
+    let scAddress;
+    let node3Instution; ;
+
+    before(async function () {
+        testConfig = new TestConfig();
+        helper = new TokenTestHelper(testConfig);
+        l1Provider = await getProvider();
+        proxyAddress = testConfig.configuration.ADDRESSES.PROXY_ADDRESS;
+        registryContract = await ethers.getContractAt("InstitutionUserRegistry", proxyAddress);
+        scAddress = testConfig.contractAddress;
+        node3Instution = testConfig.configuration.institutions[0];
+
+        metadata = {
+            admin: await helper.createMetadata(testConfig.institutions.node3.ethPrivateKey),
+            minter: await helper.createMetadata(accounts.MinterKey),
+            spender: await helper.createMetadata(accounts.Spender1Key),
+            to1: await helper.createMetadata(accounts.To1PrivateKey),
+            node4Admin: await helper.createMetadata(testConfig.institutions.node4.ethPrivateKey)
+        };
+    });
+
+    describe("Query Functions", function () {
+        it('getAllInstitutions', async function () {
+            const institutions = await registryContract.getAllInstitutions();
+            console.log("Institutions:", institutions);
+            expect(institutions[0].name).to.equal(testConfig.configuration.institutions[0].name);
+        });
+        it('should map token to institution', async function () {
+            const institution = await registryContract.getTokenInstitution(testConfig.contractAddress);
+            expect(institution[0]).to.equal(testConfig.configuration.institutions[0].name);
+        });
+        it('should map users to their managers', async function () {
+            for(institutions of testConfig.configuration.institutions){
+                console.log(`#########Institution ${institutions.name}############`);
+                for (const user of institutions.users) {
+                    const manager = await registryContract.getUserManager(user.address);
+                    console.log(`User ${user.address} manager: ${manager}`);
+                    expect(manager).to.equal(institutions.address);
+                }
+            }
+        });
+
+        it("should map institution address to their manager", async function () {
+            for (institution of testConfig.configuration.institutions){
+                console.log(`#########Institution ${institution.name}############`);
+                const result = await registryContract.getInstitution(institution.address);
+                expect(result[1]).to.equal(institution.address);
+            }
+        });
+
+        it("Should get token institution manager", async function () {
+            expect(await registryContract.getTokenInstitutionManager(scAddress)).to.equal(node3Instution.address);
+        });
+
+        it("Should map user and its institution", async function () {
+            for (institution of testConfig.configuration.institutions){
+                console.log(`#########Institution ${institution.name}############`);
+                for(user of institution.users){
+                    console.log(`User ${user.address} `)
+                    const result = await registryContract.getUserInstitution(user.address);
+                    expect(result[1]).to.equal(institution.address);
+                }
+            }
+        });
+
+        it("Should get manger Grumpkin public key", async function () {
+            const pubKey = await registryContract.getUserInstGrumpkinPubKey(node3Instution.address);
+            expect(pubKey.x).to.equal(node3Instution.publicKey.x);
+        });
+
+        it("Should check if address is institution manager", async function () {
+            for(institution of testConfig.configuration.institutions){
+                console.log(`#########Institution ${institution.name}############`);
+                expect(await registryContract.isInstitutionManager(institution.address)).to.equal(true);
+            }
+        });
+
+        it('get InstitutionCallers ',async () => {
+            let insi
+            for (institution of testConfig.configuration.institutions){
+                console.log(`#########Institution ${institution.name}############`);
+                const adminWallet = new ethers.Wallet(institution.ethPrivateKey, l1Provider);
+                insi = await ethers.getContractAt("InstitutionUserRegistry", proxyAddress, adminWallet);
+                const result = await insi.getInstitutionCallers(adminWallet.address)
+                console.log(`Institution ${institution.name} callers: ${result}`);
+
+            }
+        });
+    });
+
+
+});
+
+// 辅助函数
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
