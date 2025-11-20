e71618209d6072d5ae690d51802f258c19d67e6f
yuanqianwei <qianwei.yuan@hamsapay.com>
2025-11-20 15:29:08 +0800

    test user

--- test/qianwei/authorization_test.js
+++ test/qianwei/authorization_test.js
@@ -5,8 +5,8 @@ const {createClient} = require('../qa/token_grpc');
 const axios = require('axios');
 const { getEnvironmentConfig } = require('../../script/deploy_help.js');
 const config = getEnvironmentConfig();
-const rpcUrl = "localhost:50051";
-// const rpcUrl = "qa-node3-rpc.hamsa-ucl.com:50051";
+// const rpcUrl = "localhost:50051";
+const rpcUrl = "qa-node3-rpc.hamsa-ucl.com:50051";
 // find node3 institution
 // const node3Institution = config.institutions.find(institution => institution.name === "Node3");
 // if (!node3Institution) {
@@ -14,15 +14,15 @@ const rpcUrl = "localhost:50051";
 // }
 // const rpcUrl = node3Institution.rpcUrl;
 // const httpUrl = "http://localhost:8080";
-// const httpUrl = "http://qa-node3-http.hamsa-ucl.com:8080";
+const httpUrl = "http://qa-node3-http.hamsa-ucl.com:8080";
 
 const client = createClient(rpcUrl);
 
 const request_id = "203811416ade1a647f6dae34470de12fb1f8cee706d2d3a9bf717081b6e432a1"
 
 // admin
-// const privateKey = "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"; //N0DE3
-const privateKey = "5c231ce8344bac49d30d2c13db074ee7757c574caabfcb8349074b5486c08c7e"; //demobank
+const privateKey = "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"; //N0DE3
+// const privateKey = "5c231ce8344bac49d30d2c13db074ee7757c574caabfcb8349074b5486c08c7e"; //demobank
 // const privateKey = "81690fb141b4ae5682ad1fd73b29ae1bcc67891e93de73c6f636402deac21171"; //NODE4
 // owner 0xe46Fe251dd1d9FfC247bc0DDb6D61e4EE4416ecB
 // const privateKey = "555332672ce947d150d23a36bf3847078291f89bda7073829bb718c77d626787";
@@ -32,9 +32,9 @@ const privateKey = "5c231ce8344bac49d30d2c13db074ee7757c574caabfcb8349074b5486c0
 
 // const address = "0x8c8af239FfB9A6e93AC4b434C71a135572A1021C";
 // const address = "0x4312488937D47A007De24d48aB82940C809EEb2b";
-// const address = "0x1a245eF2f03911Bf782FBdEAe379113ff068A311";//test
+const address = "0x1a245eF2f03911Bf782FBdEAe379113ff068A311";//test
 // const address = "0x983b4ba7e42e664ddbfe4ed3e0ea07d90efcc13b";//test
-const address = "0x4312488937D47A007De24d48aB82940C809EEb2b";//test
+// const address = "0x4312488937D47A007De24d48aB82940C809EEb2b";//test
 
 async function createAuthMetadata(privateKey, messagePrefix = "login") {
     const wallet = new ethers.Wallet(privateKey);
@@ -77,9 +77,15 @@ async function testUpdateAccountStatus() {
 
 async function testRegisterAccount() {
     const metadata = await createAuthMetadata(privateKey);
+    // example: "{\"account_address\": \"0x57829d5E80730D06B1364A2b05342F44bFB70E8f\", \"account_roles\": \"admin,normal\", \"first_name\": \"John\", \"last_name\": \"Doe\", \"email\": \"john.doe@example.com\", \"phone_number\": \"(555) 123-4567\"}"
+
     const request = {
         account_address: address,
-        account_roles: "normal",//minter,admin,normal
+        account_roles: "admin,normal",//admin,normal
+        first_name: "John",
+        last_name: "Doe",
+        phone_number: "(555) 123-4567",
+        email: "john.doe@example.com",
     };
 
     try {
@@ -160,7 +166,7 @@ async function testGetAccountForHttp() {
     try {
         const headers = await createAuthHeaders(privateKey);
         // console.log('Request headers:', headers);
-        const response = await axios.post(`${httpUrl}/v1/account/query`, {
+        const response = await axios.post(`${httpUrl}/v1/account-query`, {
             account_address: address
         }, {
             headers: headers,
@@ -175,10 +181,38 @@ async function testGetAccountForHttp() {
     }
 }
 
+async function testRegisterAccountForHttp() {
+    const request = {
+        accountAddress: address,
+        accountRoles: "admin,normal",//admin,normal
+        firstName: "John",
+        lastName: "Doe",
+        phoneNumber: "(555) 123-4567",
+        email: "john.doe@example.com",
+    };
+    try {
+        const headers = await createAuthHeaders(privateKey);
+        // console.log('Request headers:', headers);
+        const response = await axios.post(`${httpUrl}/v1/account-register`,
+           request
+        , {
+            headers: headers,
+            timeout: 15000
+        });
+
+        console.log("HTTP response:", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("HTTP call failed:", error.response?.data || error.message);
+        throw error;
+    }
+}
+
 
 // testRegisterAccount().then();
-testGetAsyncAction().then();
+// testRegisterAccountForHttp().then();
+// testGetAsyncAction().then();
 // testUpdateAccountStatus().then();
 // testUpdateAccountRole().then();
 // testGetAccount().then();
-// testGetAccountForHttp().then();
+testGetAccountForHttp().then();
