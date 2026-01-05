bf3d3d01234ab28b73133d1d63672c988575c26a
jiaohui <jiaohui.shi@hamsapay.com>
2026-01-05 15:09:23 +0800

    test

--- test/qa/native_dual_minter_performance.test.js
+++ test/qa/native_dual_minter_performance.test.js
@@ -4,7 +4,7 @@ const { createClient } = require('./token_grpc');
 const accounts = require('./../../deployments/account.json');
 const grpc = require("@grpc/grpc-js");
 
-const native_token_address = "0x593B88fcbc02C960B70E8EEE468D0d6Ee7C6e64D";
+const native_token_address = "0x78e2F27aA81731861883e06204d65E9397F0DDDE";
 const rpcUrl = "dev2-node3-rpc.hamsa-ucl.com:50051";
 const client = createClient(rpcUrl);
 const RPC = 'http://dev2-ucl-l2.hamsa-ucl.com:8545';
