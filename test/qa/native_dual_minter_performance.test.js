3d6fef6ad32193f7eff7333c96f34cc90b023379
jiaohui <jiaohui.shi@hamsapay.com>
2026-01-30 12:17:29 +0800

    test

--- test/qa/native_dual_minter_performance.test.js
+++ test/qa/native_dual_minter_performance.test.js
@@ -241,7 +241,7 @@ describe.only('Native Dual Minter Split & Transfer with JSON Storage', function 
     let client, owner, minter;
     let nativeOwner, nativeMinter;
     let mintedTokens = {};
-    const total_number = 128; // Number of tokens to test
+    const total_number = 192; // Number of tokens to test
     const amount = 10000;
     const jsonFilePath = './split_tokens.json';
     
