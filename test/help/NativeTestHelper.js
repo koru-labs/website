22be6b29f0c70ecc37a4fe528f79f016dce52504
jiaohui <jiaohui.shi@hamsapay.com>
2026-02-12 17:59:26 +0800

    test

--- test/help/NativeTestHelper.js
+++ test/help/NativeTestHelper.js
@@ -3,7 +3,7 @@ const grpc = require("@grpc/grpc-js");
 
 // ==================== 常量配置 ====================
 
-const NATIVE_TOKEN_ADDRESS = "0x95abCa2D96047f91978E0F8cad91e0822f966cbd";
+const NATIVE_TOKEN_ADDRESS = "0xc35F5826875DC62fc5c51B9EE778836fac406e7b";
 const RPC_URL = "dev2-node3-rpc.hamsa-ucl.com:50051";
 // const RPC = 'http://dev2-ucl-l2.hamsa-ucl.com:8545';
 const RPC = 'http://l2-node3-native.hamsa-ucl.com:8545';
